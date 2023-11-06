2aa1f69f2d8326e3de7041542bec212c4b6d87a4
Lucas <lucas.dias@duque.g12.br>
2023-11-06 20:57:24 -0300

    Terceiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,16 @@
 import Image from "next/image";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-primary">Hello World!</h1>
-      <AlertDialog>
-        <AlertDialogTrigger>Open</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Você realmente tem certeza disso?</AlertDialogTitle>
-            <AlertDialogDescription>
-             Caso você aceite, não nos responsabilizaremos por nada ^-^
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancelar</AlertDialogCancel>
-            <AlertDialogAction>Continuar</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+
+      <div className="flex flex-row justify-center content-center gap-3"><Avatar>
+        <AvatarImage src="https://github.com/LucasThierry39.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <h3>Lucas Thierry</h3>
+      </div>
     </main>
   );
 }
