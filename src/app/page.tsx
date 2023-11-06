ecafe3d2997125e8fea15222e0c2e6a8d39b8b5f
Lucas <lucas.dias@duque.g12.br>
2023-11-06 20:39:52 -0300

    Segundo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
-import Image from 'next/image'
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className="text-primary">Hello World!</h1>
-    <Alert className='bg-blue-200'>
-      <AlertTitle className='text-blue-700 text-center text-xl'>Deu ruim</AlertTitle>
-      <AlertDescription>
-        <p className='text-blue-600 text-center'>O pc vai explodir!</p>
-      </AlertDescription>
-    </Alert>
+      <h1 className="text-primary">Hello World!</h1>
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Você realmente tem certeza disso?</AlertDialogTitle>
+            <AlertDialogDescription>
+             Caso você aceite, não nos responsabilizaremos por nada ^-^
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction>Continuar</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </main>
-  )
+  );
 }
